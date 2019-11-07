541a82263adaf656668e52dfa406655567d161ea
Charles Ferreira <charles.dev@live.com>
2019-11-07 00:41:30 -0300

    Remove código morto

--- controller/AtendimentoController.ts
+++ controller/AtendimentoController.ts
@@ -22,39 +22,38 @@ export default class AtendimentoController {
     ) {
       this.mensagemService.erro('[pessoa] campos obrigatórios');
       return;
-    } else {
-      // se tem todos os dados obrigatórios, valida
-      // se a pessoa ainda não existe no banco
+    }
+
+    // se tem todos os dados obrigatórios, valida
+    // se a pessoa ainda não existe no banco
+    if (
+      this.pessoaService.validarDadosPessoa(
+        atendimento.interessado.nomePessoa,
+        atendimento.interessado.nomeDaMae,
+        atendimento.interessado.dataDeNascimento
+      )
+    ) {
+      // valida o atendimento: se for anônimo e tiver mais de 90 dias
+      // corridos desde a ocorrência, ou se não for anônimo e tiver
+      // mais de 180 dias desde a ocorrência, exibe mensagem de erro
+      const dateDiff = Date.now() - atendimento.dataOcorrencia.getTime();
+      const numDiasOcorrencia = dateDiff / 1000 / 3600 / 24;
       if (
-        this.pessoaService.validarDadosPessoa(
-          atendimento.interessado.nomePessoa,
-          atendimento.interessado.nomeDaMae,
-          atendimento.interessado.dataDeNascimento
-        )
+        (atendimento.interessado === null && numDiasOcorrencia <= 90) ||
+        (atendimento.interessado !== null && numDiasOcorrencia <= 180)
       ) {
-        // valida o atendimento: se for anônimo e tiver mais de 90 dias
-        // corridos desde a ocorrência, ou se não for anônimo e tiver
-        // mais de 180 dias desde a ocorrência, exibe mensagem de erro
-        const dateDiff = Date.now() - atendimento.dataOcorrencia.getTime();
-        const numDiasOcorrencia = dateDiff / 1000 / 3600 / 24;
-        if (
-          (atendimento.interessado === null && numDiasOcorrencia <= 90) ||
-          (atendimento.interessado !== null && numDiasOcorrencia <= 180)
-        ) {
-          this.mensagemService.erro('[pessoa] campos obrigatórios');
-          return;
-        } else {
-          // grava a pessoa
-          this.pessoaService.gravarPessoa(atendimento.interessado);
-
-          // grava o atendimento
-          this.atendimentoService.gravarAtendimento(atendimento);
-
-          // retorna com uma mensagem de sucesso
-          this.mensagemService.sucesso('Atendimento cadastrado!');
-          return;
-        }
+        this.mensagemService.erro('[pessoa] campos obrigatórios');
+        return;
       }
+
+      // grava a pessoa
+      this.pessoaService.gravarPessoa(atendimento.interessado);
+
+      // grava o atendimento
+      this.atendimentoService.gravarAtendimento(atendimento);
+
+      // retorna com uma mensagem de sucesso
+      this.mensagemService.sucesso('Atendimento cadastrado!');
     }
   }
 }
